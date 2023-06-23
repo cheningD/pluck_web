a9ea3f621278d66888f0cf6d86d376af70efef78
Ash <ash@Admins-MacBook-Air.local>
2023-06-23 01:07:21 -0400

    e2e & backend unit test

---
+++ src/pages/[id].json.ts
@@ -0,0 +1,8 @@
+export async function get({ params }) {
+    return new Response(JSON.stringify(params.id), {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}

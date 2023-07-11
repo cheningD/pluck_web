bf6562326d6e046261d4d3aa84c8fd9e0cb79107
Chening Duker <cdduker@gmail.com>
2023-07-11 01:24:03 -0400

    try4

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,14 +1,13 @@
 import swell from 'swell-js';
-console.log('SWELL_STORE_ID before2', `---> ${'"process.env.SWELL_STORE_ID"'}`);
 
-const SWELL_STORE_ID = '"process.env.SWELL_STORE_ID"';
-const SWELL_PUBLIC_KEY = '"process.env.SWELL_PUBLIC_KEY"';
+// const SWELL_STORE_ID = '"process.env.SWELL_STORE_ID"';
+// const SWELL_PUBLIC_KEY = '"process.env.SWELL_PUBLIC_KEY"';
 
-console.log('SWELL_STORE_ID after', SWELL_STORE_ID);
+swell.init(`${process.env.SWELL_STORE_ID}`, `${process.env.SWELL_PUBLIC_KEY}`);
 
-swell.init(
-  import.meta.env.SWELL_STORE_ID || SWELL_STORE_ID,
-  import.meta.env.SWELL_PUBLIC_KEY || SWELL_PUBLIC_KEY,
-);
+// swell.init(
+//   import.meta.env.SWELL_STORE_ID || `${SWELL_STORE_ID}`,
+//   import.meta.env.SWELL_PUBLIC_KEY || `${SWELL_PUBLIC_KEY}`,
+// );
 
 export const swellClient = swell;

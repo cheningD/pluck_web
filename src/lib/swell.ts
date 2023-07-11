ec89b50ad42536230f243deba11296c91cf497ba
Chening Duker <cdduker@gmail.com>
2023-07-11 01:42:59 -0400

    try6 something weird id going on with strings

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -3,7 +3,10 @@ import swell from 'swell-js';
 // const SWELL_STORE_ID = '"process.env.SWELL_STORE_ID"';
 // const SWELL_PUBLIC_KEY = '"process.env.SWELL_PUBLIC_KEY"';
 
-swell.init(`${process.env.SWELL_STORE_ID}`, `${process.env.SWELL_PUBLIC_KEY}`);
+swell.init(
+  `${'process.env.SWELL_STORE_ID'}`,
+  `${'process.env.SWELL_PUBLIC_KEY'}`,
+);
 
 // swell.init(
 //   import.meta.env.SWELL_STORE_ID || `${SWELL_STORE_ID}`,

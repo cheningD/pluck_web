a507738e736b964133ac18e54bdc8e6ad8f17122
Chening Duker <cdduker@gmail.com>
2023-07-11 02:22:21 -0400

    Cleanup changes

--- src/pages/[id].json.ts
+++ src/pages/[id].json.ts
@@ -1,10 +1,11 @@
 import { Response } from 'node-fetch';
+export const prerender = false;
 
 export async function get({ params }) {
-    return new Response(JSON.stringify(params.id), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
+  return new Response(JSON.stringify(params.id), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }

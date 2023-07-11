cd744d21e3360705146f2aab7322ca5b75c4c30e
Chening Duker <cdduker@gmail.com>
2023-07-11 02:02:08 -0400

    try10

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,11 +1,12 @@
 import swell from 'swell-js';
 
-const SWELL_STORE_ID = 'VITE_DEFINE_SWELL_STORE_ID';
-const SWELL_PUBLIC_KEY = 'VITE_DEFINE_SWELL_PUBLIC_KEY';
-console.log('SWELL_STORE_ID--~->', SWELL_STORE_ID);
-console.log('SWELL_PUBLIC_KEY--~->', SWELL_PUBLIC_KEY.substring(0, 5));
+console.log(`SWELL_STORE_ID--~-> VITE_DEFINE_SWELL_STORE_ID <  ---`);
+console.log(`SWELL_PUBLIC_KEY--~-> VITE_DEFINE_SWELL_PUBLIC_KEY <  ---`);
 
-swell.init(`${SWELL_STORE_ID}`, `${SWELL_PUBLIC_KEY}`);
+swell.init(
+  `${'VITE_DEFINE_SWELL_STORE_ID'}`,
+  `${'VITE_DEFINE_SWELL_PUBLIC_KEY'}`,
+);
 
 // swell.init(
 //   import.meta.env.SWELL_STORE_ID || `${SWELL_STORE_ID}`,

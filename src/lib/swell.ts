a72ecf9adbc794d66003237ee13d24ce9315b059
Chening Duker <cdduker@gmail.com>
2023-07-11 01:08:09 -0400

    use vite define for env vars on cloudflare

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,18 +1,10 @@
 import swell from 'swell-js';
 const SWELL_STORE_ID = 'process.env.SWELL_STORE_ID';
 const SWELL_PUBLIC_KEY = 'process.env.SWELL_PUBLIC_KEY';
-console.log('SWELL_PUBLIC_KEY', SWELL_PUBLIC_KEY);
-console.log('SWELL_STORE_ID', SWELL_STORE_ID);
-console.log('SWELL_STORE_ID -original', import.meta.env.SWELL_STORE_ID);
-console.log('SWELL_PUBLIC_KEY -original', import.meta.env.SWELL_PUBLIC_KEY);
-const str = import.meta.env.SWELL_PUBLIC_KEY;
-console.log(
-  'SWELL_PUBLIC_KEY',
-  str.substring(0, 2) +
-    'X'.repeat(str.length - 4) +
-    str.substring(str.length - 2),
-);
 
-swell.init(import.meta.env.SWELL_STORE_ID, import.meta.env.SWELL_PUBLIC_KEY);
+swell.init(
+  import.meta.env.SWELL_STORE_ID || SWELL_STORE_ID,
+  import.meta.env.SWELL_PUBLIC_KEY || SWELL_PUBLIC_KEY,
+);
 
 export const swellClient = swell;

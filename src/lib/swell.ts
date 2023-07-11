18907b402fbfa2bf1abf317a6f38b5998b0222e3
Chening Duker <cdduker@gmail.com>
2023-07-11 00:06:10 -0400

    Turn pre-rendering on dynamic routes off. Debug swell vars

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -2,7 +2,12 @@ import swell from 'swell-js';
 
 console.log('SWELL_STORE_ID', import.meta.env.SWELL_STORE_ID);
 const str = import.meta.env.SWELL_PUBLIC_KEY;
-console.log('SWELL_PUBLIC_KEY', str.substring(0, 2) + 'X'.repeat(str.length - 4) + str.substring(str.length - 2););
+console.log(
+  'SWELL_PUBLIC_KEY',
+  str.substring(0, 2) +
+    'X'.repeat(str.length - 4) +
+    str.substring(str.length - 2),
+);
 
 swell.init(import.meta.env.SWELL_STORE_ID, import.meta.env.SWELL_PUBLIC_KEY);
 
---
+++ src/pages/[id].json.ts
@@ -0,0 +1,12 @@
+import { Response } from 'node-fetch';
+
+export const prerender = false;
+
+export async function get({ params }) {
+  return new Response(JSON.stringify(params.id), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}

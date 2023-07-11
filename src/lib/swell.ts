1dad51eef76d740b4d2d42f333aa9005cd198620
Chening Duker <cdduker@gmail.com>
2023-07-11 01:16:52 -0400

    try 2

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,6 +1,11 @@
 import swell from 'swell-js';
-const SWELL_STORE_ID = "'process.env.SWELL_STORE_ID'";
-const SWELL_PUBLIC_KEY = "'process.env.SWELL_PUBLIC_KEY'";
+console.log('SWELL_STORE_ID before2', `---> ${'"process.env.SWELL_STORE_ID"'}`);
+console.log('SWELL_STORE_ID before', `--> ${'process.env.SWELL_STORE_ID'}`);
+
+const SWELL_STORE_ID = 'process.env.SWELL_STORE_ID';
+const SWELL_PUBLIC_KEY = 'process.env.SWELL_PUBLIC_KEY';
+
+console.log('SWELL_STORE_ID after', SWELL_STORE_ID);
 
 swell.init(
   import.meta.env.SWELL_STORE_ID || SWELL_STORE_ID,
